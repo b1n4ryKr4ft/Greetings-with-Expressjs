51809355de55fd662c814348368dc590b50f9664
Wiseman Mabusela <95911962+Wiseman930@users.noreply.github.com>
2022-08-17 00:25:34 +0200

    update

--- index.js
+++ index.js
@@ -49,3 +49,4 @@ app.get("/actions", function (req, res) {
   app.listen(process.env.PORT || 3000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
     });
+    
